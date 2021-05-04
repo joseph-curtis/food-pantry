a2a53f4093274b673d8d2dfca250d77de380c7e9
Joseph Curtis <joe.curtis@pcc.edu>
2021-05-03 23:30:02 -0700

    added new message testData

--- src/sql/addTestData.sql
+++ src/sql/addTestData.sql
@@ -1,57 +1,66 @@
 /*
 ********************************************************************************
-//  INSERTs test data including managers & workers
+//  INSERT test data including people and messages
 //
 //  Software:	Microsoft SQL Server Management Studio - (Transact SQL)
 //				SQL Server 2012 architecture
-//  version:	04.24.2021
+//  version:	05.03.2021
 //  Notes:      copy/paste this in SQL Server Management Studio and execute
 ********************************************************************************
 */
 USE cis234a_team_JK_LOL;
 
-DECLARE @manager_password NVARCHAR(64) = 'password';
-DECLARE @manager_hash VARBINARY(128);
-SET @manager_hash = HASHBYTES('SHA2_256', @manager_password);
+/* =========== */
+/* add people: */
+/* =========== */
 
-DECLARE @worker_password NVARCHAR(64) = 'workpassword';
-DECLARE @worker_hash VARBINARY(128);
-SET @worker_hash = HASHBYTES('SHA2_256', @worker_password);
-
-DECLARE @subscriber_password NVARCHAR(64) = 'studentpassword';
-DECLARE @subscriber_hash VARBINARY(128);
-SET @subscriber_hash = HASHBYTES('SHA2_256', @subscriber_password);
-
-INSERT INTO PERSON (PK_Person_ID, firstname, lastname, username,
-                    password_hash, email, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, role)
 VALUES (
-    (SELECT ISNULL(MAX(PK_Person_ID), 0) + 1 FROM PERSON)
-    , 'test'
+    'manager'
     , 'manager'
     , 'testmanager'
-    , @manager_hash
+    , HASHBYTES('SHA2_256', 'managerpassword')
     , 'teamcjklol@gmail.com'
     , 'Manager'
 );
-INSERT INTO PERSON (PK_Person_ID, firstname, lastname, username,
-                    password_hash, email, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, role)
 VALUES (
-    (SELECT ISNULL(MAX(PK_Person_ID), 0) + 1 FROM PERSON)
-    , 'test'
+    'worker'
     , 'worker'
     , 'testworker'
-    , @worker_hash
+    , HASHBYTES('SHA2_256', 'workerpassword')
     , 'teamcjklol@gmail.com'
     , 'Worker'
 );
-INSERT INTO PERSON (PK_Person_ID, firstname, lastname, username,
-                    password_hash, email, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash,
+					email, phone, role)
 VALUES (
-    (SELECT ISNULL(MAX(PK_Person_ID), 0) + 1 FROM PERSON)
-    , 'test'
+    'student'
     , 'student'
-    , 'testworker'
-    , @subscriber_hash
+    , 'teststudent'
+    , HASHBYTES('SHA2_256', 'studentpassword')
     , 'teamcjklol@gmail.com'
-    , 'Subscriber'
+	, 5035551111
+    , 'Student'
+);
+
+/* ============================= */
+/* add messages with recipients: */
+/* ============================= */
+
+-- this is important to retreive the same PK_MessageID for RECIPIENT table:
+DECLARE @messageDatetime smalldatetime = CAST(GETDATE() AS smalldatetime);
+
+INSERT INTO MESSAGE (FK_FromPerson_ID, subject, textbody, datetime)
+VALUES (
+	(SELECT PK_Person_ID FROM PERSON WHERE username = 'teststudent')
+	, 'test message'
+	, 'this is the <b>text body</b> of the message. This is just a test.'
+	, @messageDatetime
+);
+INSERT INTO RECIPIENT (FK_ToPerson_ID, FK_Message_ID, to_email)
+VALUES (
+	(SELECT PK_Person_ID FROM PERSON WHERE username = 'teststudent')
+	, (SELECT PK_Message_ID FROM MESSAGE WHERE datetime = @messageDatetime)
+	, 'canBeAny@email.here'
 );
