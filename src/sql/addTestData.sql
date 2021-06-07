797c917e779ebb3b08fe1a95ff07e57fe785194b
Joseph Curtis <3443776+joseph-curtis@users.noreply.github.com>
2021-06-07 14:58:39 -0700

    Updated to add test case

--- src/sql/addTestData.sql
+++ src/sql/addTestData.sql
@@ -14,7 +14,7 @@ USE cis234a_team_JK_LOL;
 /* add people: */
 /* =========== */
 
-INSERT INTO PERSON (firstname, lastname, username, password_hash, email, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, role, activated, receive_email, receive_sms)
 VALUES (
     'Cosmo'
     , 'Spacely'
@@ -22,8 +22,11 @@ VALUES (
     , HASHBYTES('SHA2_256', CONVERT(NVARCHAR(MAX), 'managerpassword'))
     , 'teamcjklol@gmail.com'
     , 'Manager'
+    , 1
+    , 0
+    , 0
 );
-INSERT INTO PERSON (firstname, lastname, username, password_hash, email, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, role, activated, receive_email, receive_sms)
 VALUES (
     'George'
     , 'Jetson'
@@ -31,8 +34,11 @@ VALUES (
     , HASHBYTES('SHA2_256', CONVERT(NVARCHAR(MAX), 'workerpassword'))
     , 'teamcjklol@gmail.com'
     , 'Worker'
+    , 1
+    , 0
+    , 0
 );
-INSERT INTO PERSON (firstname, lastname, username, password_hash, email, phone, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, phone, role, activated, receive_email, receive_sms)
 VALUES (
     'Susan'
     , 'Studensky'
@@ -41,8 +47,11 @@ VALUES (
     , 'test.student@pcc.edu'
 	, 5035551111
     , 'Student'
+    , 1
+    , 1
+    , 0
 );
-INSERT INTO PERSON (firstname, lastname, username, password_hash, email, phone, role)
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, phone, role, activated, receive_email, receive_sms)
 VALUES (
     'Joseph'
     , 'Curtis'
@@ -51,6 +60,22 @@ VALUES (
     , 'joe.curtis@pcc.edu'
 	, 9712279173
     , 'Worker'
+    , 1
+    , 1
+    , 0
+);
+INSERT INTO PERSON (firstname, lastname, username, password_hash, email, phone, role, activated, receive_email, receive_sms)
+VALUES (
+    'Jack'
+    , 'Dillon'
+    , 'Jack'
+    , HASHBYTES('SHA2_256', CONVERT(NVARCHAR(MAX), 'password'))
+    , 'test.email@pcc.edu'
+	, 9257681212
+    , 'Student'
+    , 1
+    , 1
+    , 1
 );
 
 /* ============================= */
